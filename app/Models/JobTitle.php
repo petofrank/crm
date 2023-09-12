1b0e1d7b9dc3c8e670a2684bbcb736a28bc5c8f6
PeterFrank <peto.frank@gmail.com>
2023-09-12 14:53:57 +0200

    added model fillable and migrations

---
+++ app/Models/JobTitle.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobTitle extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'uuid',
+        'name',
+    ];
+}
